05e1bfaeb23f4acca8f67d4855a7782afe72ae26
apostolov <apostolov@knu.ua>
2024-10-18 16:10:12 +0300

    added builder pattern

---
+++ Builder/builder_patters/bin/builder_patters.dart
@@ -0,0 +1,109 @@
+class Car {
+  String? engine;
+  int? seats;
+  String? body;
+  int? doors;
+  void showDetails() {
+    print('Car with engine: $engine, body: $body, doors: $doors');
+  }
+}
+
+abstract class CarBuilder {
+  void setEngine();
+  void setSeats();
+  void setBody();
+  void setDoors();
+  Car getCar();
+}
+
+class SportsCarBuilder implements CarBuilder {
+  Car _car = Car();
+
+  @override
+  void setEngine() {
+    _car.engine = 'V8 Engine';
+  }
+
+  @override
+  void setBody() {
+    _car.body = 'Sports Body';
+  }
+
+  @override
+  void setDoors() {
+    _car.doors = 2;
+  }
+
+  @override
+  void setSeats() {
+    _car.seats = 2;
+  }
+
+  @override
+  Car getCar() {
+    return _car;
+  }
+}
+
+class WagonCarBuilder implements CarBuilder {
+  Car _car = Car();
+
+  @override
+  void setEngine() {
+    _car.engine = 'V6';
+  }
+
+  @override
+  void setBody() {
+    _car.body = 'Long Body';
+  }
+
+  @override
+  void setDoors() {
+    _car.doors = 5;
+  }
+
+  @override
+  void setSeats() {
+    _car.seats = 5;
+  }
+
+  @override
+  Car getCar() {
+    return _car;
+  }
+}
+
+class Director {
+  CarBuilder? _carBuilder;
+
+  void setBuilder(CarBuilder builder) {
+    _carBuilder = builder;
+  }
+
+  Car buildCar() {
+    _carBuilder?.setEngine();
+    _carBuilder?.setBody();
+    _carBuilder?.setSeats();
+    _carBuilder?.setDoors();
+    return _carBuilder!.getCar();
+  }
+}
+
+void main() {
+  final director = Director();
+
+  // Побудова спортивного автомобіля
+  final sportsCarBuilder = SportsCarBuilder();
+  director.setBuilder(sportsCarBuilder);
+  final sportsCar = director.buildCar();
+  sportsCar.showDetails();
+  // Виведе: Car with engine: V8 Engine, body: Sports Body, doors: 2
+
+  // Побудова позашляховика
+  final wagonCarBuilder = WagonCarBuilder();
+  director.setBuilder(wagonCarBuilder);
+  final suvCar = director.buildCar();
+  suvCar.showDetails();
+  // Виведе: Car with engine: V6 Engine, body: SUV Body, doors: 4
+}
---
+++ Builder/builder_patters/test/builder_patters_test.dart
@@ -0,0 +1,8 @@
+import 'package:builder_patters/builder_patters.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
