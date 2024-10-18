0feaff347d3f485b5d533a4fbd078d12825171dc
apostolov <apostolov@knu.ua>
2024-10-18 13:57:05 +0300

    updated factory method

--- Factory/factorymethod/bin/factorymethod.dart
+++ Factory/factorymethod/bin/factorymethod.dart
@@ -1,5 +1,62 @@
-import 'package:factorymethod/factorymethod.dart' as factorymethod;
+//created general interface
 
-void main(List<String> arguments) {
-  print('Hello world: ${factorymethod.calculate()}!');
+abstract class Delivery {
+  void deliveryMethod();
+}
+
+class Truck implements Delivery {
+  @override
+  void deliveryMethod() {
+    print('delivery by road');
+  }
+}
+
+class Plane implements Delivery {
+  @override
+  void deliveryMethod() {
+    print('delivery by air');
+  }
+}
+
+class Ship implements Delivery {
+  @override
+  void deliveryMethod() {
+    print('delivety by water');
+  }
+}
+
+abstract class Logistics {
+  Delivery createDeliviryMethod();
+
+  void planDelivery() {
+    final delivery = createDeliviryMethod();
+    delivery.deliveryMethod();
+  }
+}
+
+class RoadLogistics extends Logistics {
+  @override
+  Delivery createDeliviryMethod() {
+    return Truck();
+  }
+}
+
+class AirLogistics extends Logistics {
+  @override
+  Delivery createDeliviryMethod() {
+    return Plane();
+  }
+}
+
+class WaterLogistics extends Logistics {
+  @override
+  Delivery createDeliviryMethod() {
+    return Ship();
+  }
+}
+
+void main() {
+  Logistics logistics;
+  logistics = AirLogistics();
+  logistics.planDelivery();
 }
--- Factory/factorymethod/test/factorymethod_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:factorymethod/factorymethod.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
